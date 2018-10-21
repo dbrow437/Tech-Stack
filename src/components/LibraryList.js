cc39842dacd060a4a1b6e91969f81ec5b6d6fee5
Damon Brown <dbrow437@gmail.com>
2018-10-21 11:46:14 -0700

    code fixed by passing through mapStateToProps in connect

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -5,15 +5,16 @@ import ListItem from "./ListItem";
 
 class LibraryList extends Component {
   renderItem(library) {
-    return <ListItem library={library.item} />;
+    return <ListItem library={library} />;
   }
 
   render() {
+    console.log(this.props);
     return (
       <FlatList
         data={this.props.libraries}
         renderItem={this.renderItem}
-        keyExtractor={library => library.id.toString()}
+        keyExtractor={library => library.id}
       />
     );
   }
@@ -23,4 +24,4 @@ const mapStateToProps = state => {
   return { libraries: state.libraries };
 };
 
-export default connect()(LibraryList);
+export default connect(mapStateToProps)(LibraryList);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { CardSection } from "./common";
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
-    console.log(this.props.library);
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.item.title}</Text>
-      </CardSection>
+      <View>
+        <CardSection>
+          <Text style={titleStyle}>{this.props.library.item.title}</Text>
+        </CardSection>
+      </View>
     );
   }
 }
