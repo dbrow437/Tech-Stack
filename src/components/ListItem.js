d9cd8b445cca1e96c2db0cb0d5be76c76084f36b
Damon Brown <dbrow437@gmail.com>
2018-10-22 19:22:58 -0700

    animations added and redux working

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from "react-native";
+import { connect } from "react-redux";
 import { CardSection } from "./common";
+import * as actions from "../actions";
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.item.description}</Text>
+        </CardSection>
+      );
+    }
+  }
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library.item;
 
     return (
-      <View>
-        <CardSection>
-          <Text style={titleStyle}>{this.props.library.item.title}</Text>
-        </CardSection>
-      </View>
+      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -23,4 +50,12 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+  return { expanded };
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(ListItem);
